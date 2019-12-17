c47c1945309d737c275f911c4f46b5f7ba4a3330
GloriaD00 <GloriaD00@users.noreply.github.com>
2019-12-17 09:56:58 +0100

    config example

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+return [
+
+    'database'=>[
+
+        'name'=>'GetSD',
+        'user'=>'root',
+        'password'=>'',
+        'connection'=>'mysql:host=localhost',
+        'port'=>'8000',
+        'options'=>[
+
+            PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+        ]
+
+
+    ]
+
+];
+
